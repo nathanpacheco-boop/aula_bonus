37be85b8797adf02356feaa19d8b000dd5b3bcdd
nathanpacheco-boop <nathan_t_pacheco@estudante.sesisenai.org.br>
2026-04-13 08:48:27 -0300

    finalizado codigo javascript

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+
+const sessaoAtiva = localStorage.getItem("sessao");
+
+if (sessaoAtiva !== null) {
+  mostrarAreaLogado(sessaoAtiva);
+}
 function cadastrar () {
     const usuario = document.getElementById("usuario").value.trim();
     const senha = document.getElementById("senha").value; 
@@ -19,7 +25,37 @@ function cadastrar () {
     }
     localStorage.setItem("usuario_" + uauario, senha);
     exibirmensagem("conta criada com sucesso! Faça login.", "ok");
+    
+    function entrar(){
+const usuario= document.getElementById("usuario").value.trim();
+const senha= document.getElementById("senha").value;
 
+if (usuario === ""|| senha === ""){
+    exibirMensagem("Preencha usuário e senha.", "erro");
+    return;
+}
+
+const senhaSalva = localStorage.getItem("usuario_" + usuario);
+
+if (senhaSalva === null){
+    exibirMensagem("Usuário não encontrado.", "erro");
+    return;
+}
+
+if (senhaSalva !== senha){
+    exibirMensagem("Senha incorreta.", "erro");
+    return;
+}
+
+localStorage.setItem("sessao", usuario);
+mostarAreaLogado(usuario);
+    }
+
+    function sair(){
+        localStorage.removeItem("sessao");
+        mostarAreaLogin();
+    }
+    
     function exibirMensagem(texto, tipo) {
         const mensagem = document.getElementById("mensagem");
 
@@ -28,4 +64,14 @@ function cadastrar () {
     mensagem.style.display = "block";
     
     }
+    function mostarAreaLogado(usuario){
+        document.getElementById("area-login").style.display = "nome";
+        document.getElementById("area-logado").style.display= "block";
+        document.getElementById("texto-bem-vindo").textContent= "Olá" + usuario + ";";
+    }
+
+     function mostarAreaLogin(usuario){
+        document.getElementById("area-login").style.display = "block";
+        document.getElementById("area-logado").style.display= "none";
+     }
 }
